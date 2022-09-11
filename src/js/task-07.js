6bdd284d3339faf2ba0938d8385d39ef13b55edc
Ivan <104781156+IvanTol@users.noreply.github.com>
2022-09-11 12:02:50 +0300

    finish

---
+++ src/js/task-07.js
@@ -0,0 +1,19 @@
+const inputRef = document.querySelector("#font-size-control");
+const spanRef = document.querySelector("#text");
+
+// console.dir(spanRef);
+
+// spanRef.style.fontSize = inputRef.value + "px";
+// inputRef.addEventListener('input', () => {
+//     // console.log(inputRef.value);
+//     spanRef.style.fontSize = inputRef.value + "px";
+// })
+
+const ref = function () {
+  spanRef.style.fontSize = inputRef.value + "px";
+};
+ref();
+
+inputRef.addEventListener("input", () => {
+  ref();
+});
